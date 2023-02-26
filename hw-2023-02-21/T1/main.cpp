9178ef4aee8cf2f7847b619298681d86b8cce74c
karenmkrtumyan7 <karenmkrtumyan7@gmail.com>
2023-02-26 22:11:31 +0400

    resolved

--- hw-2023-02-21/T1/main.cpp
+++ hw-2023-02-21/T1/main.cpp
@@ -4,43 +4,61 @@
 
 std::string src = "";
 std::string dest = "";
+std::string dest_parts[4];
 pthread_mutex_t mutex;
+bool finished[4] = { false };
 
 void* copy_string(void* arg) {
-	int id = *(int*)arg;
-	int size = src.size() / 4;
-	int l = id * size;
-	int r = id < 3 ? (id + 1) * size : src.size();
-	std::string part = src.substr(l, r - l);
+    int id = *(int*)arg;
+    int size = src.size() / 4;
+    int l = id * size;
+    int r = id < 3 ? (id + 1) * size : src.size();
+    std::string part = src.substr(l, r - l);
 
-	pthread_mutex_lock(&mutex);
-	dest += part;
-	pthread_mutex_unlock(&mutex);
+    pthread_mutex_lock(&mutex);
+    dest_parts[id] = part;
+    finished[id] = true;
+    pthread_mutex_unlock(&mutex);
 
-	return 0;
+    return 0;
+}
+
+void* combine_strings(void* arg) {
+    while (!finished[0] || !finished[1] || !finished[2] || !finished[3]) {}
+
+    for (int i = 0; i < 4; i++) {
+        dest += dest_parts[i];
+    }
+
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
-	if (argc < 2) {
-		std::cout << "Invalid \n"; 
-		return 1;
-	}
+    if (argc < 2) {
+        std::cout << "Invalid \n"; 
+        return 1;
+    }
 
-	pthread_mutex_init(&mutex, NULL);
-	pthread_t threads[4];
-	int ids[4];
-	src = argv[1];
+    pthread_mutex_init(&mutex, NULL);
+    pthread_t threads[5];
+    int ids[4];
+    src = argv[1];
 
-	for (int i = 0; i < 4; i++) {
-		ids[i] = i;
-		pthread_create(&threads[i], NULL, copy_string, &ids[i]);
-	}
+    pthread_create(&threads[4], NULL, combine_strings, NULL);
 
-	for (int i = 0; i < 4; i++) {
-		pthread_join(threads[i], NULL);
-	}
+    for (int i = 0; i < 4; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, copy_string, &ids[i]);
+    }
 
-	pthread_mutex_destroy(&mutex);
-	std::cout << dest << "\n";
-	return 0;
+    for (int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_join(threads[4], NULL);
+    pthread_mutex_destroy(&mutex);
+
+    std::cout << dest << "\n";
+    return 0;
 }
+
